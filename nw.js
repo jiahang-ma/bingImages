bbba9de22ee155d50315b488114118efa7a60bc8
hang1989 <419917737@qq.com>
2018-10-16 09:22:40 +0800

    app(chore):修改了nw.js指向app.js文件路径错误问题

--- nw.js
+++ nw.js
@@ -4,7 +4,7 @@ let path = require('path');
 let svc = new Service({
   name: 'node_bing', //服务名称
   description: 'bing每日一图', //描述
-  script: __filename //nodejs项目要启动的文件路径
+  script: 'D:/work/bing/build/app/app.js' //nodejs项目要启动的文件路径
 });
 
 svc.on('install', () => {
