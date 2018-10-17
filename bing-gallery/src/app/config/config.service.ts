952f078747be74fcb0ea1ceba2f08f9814dc5f61
hang1989 <419917737@qq.com>
2018-10-17 15:23:42 +0800

    app(chore):添加了后端图片API接口

--- bing-gallery/src/app/config/config.service.ts
+++ bing-gallery/src/app/config/config.service.ts
@@ -4,23 +4,23 @@ import {environment} from '../../environments/environment';
 @Injectable()
 
 export class ConfigService {
-  hostname = 'http://192.168.1.199:8080/api';
-  imgHostname = 'http://mhgl.bjtckj.cn:9094';
-  shareHostname = ''; // 这个应该使用当前项目IP地址
+    hostname = 'http://192.168.1.199:8081';
+    imgHostname = '';
+    shareHostname = ''; // 这个应该使用当前项目IP地址
 
-  constructor() {
-    if (environment.production) {
-      this.setProductionConfig();
+    constructor() {
+        if (environment.production) {
+            this.setProductionConfig();
+        }
     }
-  }
 
-  /**
-   * 此方法禁止修改
-   */
-  private setProductionConfig() {
-    this.hostname = 'http://192.168.1.199:8080/api';
-    this.imgHostname = 'http://mhgl.bjtckj.cn:9094';
-    this.shareHostname = ''; // 这个应该使用当前项目IP地址
-  }
+    /**
+     * 此方法禁止修改
+     */
+    private setProductionConfig() {
+        this.hostname = 'http://192.168.1.199:8081';
+        this.imgHostname = '';
+        this.shareHostname = ''; // 这个应该使用当前项目IP地址
+    }
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import {download} from './download/upload';
+import { download } from './download/upload';
+const service = require('./service/app');
+
 
 download(); // 下载图片开始
+service(); // 后端图片服务启动
 
 // todo 添加一个service提供图片接口
