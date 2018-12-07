8a3578972142a9ca04ebe54c3f17bc14ac63bed0
hang1989 <419917737@qq.com>
2018-12-07 10:43:23 +0800

    service(feat):添加了获取图片名称接口

--- src/app/service/database/imagesData.js
+++ src/app/service/database/imagesData.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectBaseUrl = path.join(__dirname, '../../../..');
+const targetImgPath = projectBaseUrl + '/images';
+
+const imgs = [];
+
+
+fs.readdirSync(targetImgPath).filter((f) => {
+    return f.endsWith('.jpg') || f.endsWith('.png') || f.endsWith('.jpeg');
+}).forEach((f) => {
+    imgs.push(f);
+});
+
+
 const data = {
     "msg": "success",
     "result": "00000000",
-    "data": [] // 自定义需要的数据
+    "data": imgs // 自定义需要的数据
 
 }
 
-module.exports = {data}; // 相当于es6的 export node.js目前只支持此种
+module.exports = {
+    data
+}; // 相当于es6的 export node.js目前只支持此种
