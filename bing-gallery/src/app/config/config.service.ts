5a5fdc06803aa0c9966b5e69f3e652284508a657
hang1989 <419917737@qq.com>
2018-12-07 12:01:11 +0800

    app(chore):现在从后端接口获取图片路径

--- bing-gallery/src/app/config/config.service.ts
+++ bing-gallery/src/app/config/config.service.ts
@@ -4,8 +4,8 @@ import {environment} from '../../environments/environment';
 @Injectable()
 
 export class ConfigService {
-    hostname = 'http://192.168.1.199:8081';
-    imgHostname = '';
+    hostname = 'http://192.168.1.199:8888';
+    imgHostname = 'http://192.168.1.199:8081';
     shareHostname = ''; // 这个应该使用当前项目IP地址
 
     constructor() {
@@ -18,8 +18,8 @@ export class ConfigService {
      * 此方法禁止修改
      */
     private setProductionConfig() {
-        this.hostname = 'http://192.168.1.199:8081';
-        this.imgHostname = '';
+        this.hostname = 'http://192.168.1.199:8888';
+        this.imgHostname = 'http://192.168.1.199:8081';
         this.shareHostname = ''; // 这个应该使用当前项目IP地址
     }
 
--- bing-gallery/src/app/light-gallery/light-gallery.component.ts
+++ bing-gallery/src/app/light-gallery/light-gallery.component.ts
@@ -1,71 +1,93 @@
 import {Component, OnInit, ElementRef, AfterViewInit} from '@angular/core';
-
-interface Image {
-  title: string;
-  path: string;
-}
+import {HttpService} from '@http';
+import {ConfigService} from '../config/config.service';
 
 @Component({
-  templateUrl: './light-gallery.component.html',
-  styleUrls: ['./light-gallery.component.css']
+    templateUrl: './light-gallery.component.html',
+    styleUrls: ['./light-gallery.component.css']
 })
 export class LightGalleryComponent implements OnInit, AfterViewInit {
 
-  constructor(private elementRef: ElementRef) {
-  }
+    imagePath: string;
+    images: string[];
+    imgHostname: string;
 
-  images: Array<Image> = [
-    {
-      title: '美泉宫花园的秋日景色， 奥地利维也纳',
-      path: '1.jpg'
-    },
-    {
-      title: '1846年8月10日成立的史密森尼学会',
-      path: '2.jpg'
-    },
-    {
-      title: '安东尼奥港的景色，牙买加',
-      path: '3.jpg'
-    },
-    {
-      title: '费城爱心公园内的罗伯特·印第安纳的雕塑Love',
-      path: '4.jpg'
-    },
-    {
-      title: '节日期间爱丁堡城堡上空的烟花',
-      path: '5.jpg'
-    },
-    {
-      title: '凯恩戈姆山国家公园内的苏格兰野猫，苏格兰',
-      path: '6.jpg'
-    },
-    {
-      title: '瓦伊纳皮克丘山周围的云雾，秘鲁',
-      path: '7.jpg'
-    },
-    {
-      title: '在恶地国家公园骑行的飞车党，美国南达科他州',
-      path: '8.jpg'
+    constructor(private elementRef: ElementRef,
+                private http: HttpService,
+                private config: ConfigService) {
+        this.imagePath = '/images';
+        this.imgHostname = config.imgHostname;
     }
-  ];
-  imgHostname = '../../../assets/previews/';
 
-  openLightGallery() {
-    const firstImg = this.elementRef.nativeElement.querySelector('.img-item');
-    firstImg.click();
-  }
 
-  private lightGalleryInit() {
-    lightGallery(document.getElementById('aniimated-thumbnials'), {
-      thumbnail: true
-    });
-  }
+    fetchImagesData() {
+        this.http.get(this.imagePath)
+            .then(res => {
+                this.images = res.data.map(i => {
+                    return {path: this.imgHostname + '/' + i, title: i};
+                });
+                console.log(this.images);
+                setTimeout(() => {
+                    this.lightGalleryInit();
+                }, 0);
+            })
+            .catch(err => {
 
-  ngOnInit() {
-  }
+            });
+    }
 
-  ngAfterViewInit() {
-    this.lightGalleryInit();
-  }
+    // images: Array<Image> = [
+    //     {
+    //         title: '美泉宫花园的秋日景色， 奥地利维也纳',
+    //         path: '1.jpg'
+    //     },
+    //     {
+    //         title: '1846年8月10日成立的史密森尼学会',
+    //         path: '2.jpg'
+    //     },
+    //     {
+    //         title: '安东尼奥港的景色，牙买加',
+    //         path: '3.jpg'
+    //     },
+    //     {
+    //         title: '费城爱心公园内的罗伯特·印第安纳的雕塑Love',
+    //         path: '4.jpg'
+    //     },
+    //     {
+    //         title: '节日期间爱丁堡城堡上空的烟花',
+    //         path: '5.jpg'
+    //     },
+    //     {
+    //         title: '凯恩戈姆山国家公园内的苏格兰野猫，苏格兰',
+    //         path: '6.jpg'
+    //     },
+    //     {
+    //         title: '瓦伊纳皮克丘山周围的云雾，秘鲁',
+    //         path: '7.jpg'
+    //     },
+    //     {
+    //         title: '在恶地国家公园骑行的飞车党，美国南达科他州',
+    //         path: '8.jpg'
+    //     }
+    // ];
+
+    openLightGallery() {
+        const firstImg = this.elementRef.nativeElement.querySelector('.img-item');
+        firstImg.click();
+    }
+
+    private lightGalleryInit() {
+        lightGallery(document.getElementById('aniimated-thumbnials'), {
+            thumbnail: true
+        });
+    }
+
+    ngOnInit() {
+        this.fetchImagesData();
+    }
+
+    ngAfterViewInit() {
+        // this.lightGalleryInit();
+    }
 
 }
