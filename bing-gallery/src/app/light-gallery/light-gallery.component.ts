9249e9d9ff0a87c304a0eb33a852f6832b948f5e
hang1989 <419917737@qq.com>
2018-12-07 13:22:03 +0800

    app(chore):删除了一些无用代码

--- bing-gallery/src/app/light-gallery/light-gallery.component.ts
+++ bing-gallery/src/app/light-gallery/light-gallery.component.ts
@@ -9,7 +9,7 @@ import {ConfigService} from '../config/config.service';
 export class LightGalleryComponent implements OnInit, AfterViewInit {
 
     imagePath: string;
-    images: string[];
+    images: { path: string, title: string }[];
     imgHostname: string;
 
     constructor(private elementRef: ElementRef,
@@ -19,14 +19,12 @@ export class LightGalleryComponent implements OnInit, AfterViewInit {
         this.imgHostname = config.imgHostname;
     }
 
-
     fetchImagesData() {
         this.http.get(this.imagePath)
             .then(res => {
                 this.images = res.data.map(i => {
                     return {path: this.imgHostname + '/' + i, title: i};
                 });
-                console.log(this.images);
                 setTimeout(() => {
                     this.lightGalleryInit();
                 }, 0);
@@ -35,41 +33,6 @@ export class LightGalleryComponent implements OnInit, AfterViewInit {
 
             });
     }
-
-    // images: Array<Image> = [
-    //     {
-    //         title: '美泉宫花园的秋日景色， 奥地利维也纳',
-    //         path: '1.jpg'
-    //     },
-    //     {
-    //         title: '1846年8月10日成立的史密森尼学会',
-    //         path: '2.jpg'
-    //     },
-    //     {
-    //         title: '安东尼奥港的景色，牙买加',
-    //         path: '3.jpg'
-    //     },
-    //     {
-    //         title: '费城爱心公园内的罗伯特·印第安纳的雕塑Love',
-    //         path: '4.jpg'
-    //     },
-    //     {
-    //         title: '节日期间爱丁堡城堡上空的烟花',
-    //         path: '5.jpg'
-    //     },
-    //     {
-    //         title: '凯恩戈姆山国家公园内的苏格兰野猫，苏格兰',
-    //         path: '6.jpg'
-    //     },
-    //     {
-    //         title: '瓦伊纳皮克丘山周围的云雾，秘鲁',
-    //         path: '7.jpg'
-    //     },
-    //     {
-    //         title: '在恶地国家公园骑行的飞车党，美国南达科他州',
-    //         path: '8.jpg'
-    //     }
-    // ];
 
     openLightGallery() {
         const firstImg = this.elementRef.nativeElement.querySelector('.img-item');
@@ -87,7 +50,7 @@ export class LightGalleryComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
-        // this.lightGalleryInit();
+
     }
 
 }
